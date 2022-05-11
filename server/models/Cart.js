b35904ea694380016a3fdb30dc158fed276ca47b
Amit <amitshuu@gmail.com>
2022-05-11 15:45:24 +0300

    Mongoose models

---
+++ server/models/Cart.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const CartSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  products: [
+    {
+      productId: { type: String },
+      quantity: { type: Number, default: 1 },
+    },
+  ],
+});
