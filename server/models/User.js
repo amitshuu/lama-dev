08cf3e0c39c9a6b35e6329af04da7001c342d2c7
Amit <amitshuu@gmail.com>
2022-05-12 15:56:46 +0300

    Added methods

--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,31 @@
+import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema(
   {
     username: { type: String, unique: true, required: true },
     email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true, select: false },
     isAdmin: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
+
+UserSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+  const salt = await bcryptjs.genSalt(10);
+  this.password = await bcryptjs.hash(this.password, salt);
+});
+
+UserSchema.methods.comparePassword = async function (password) {
+  return await bcryptjs.compare(password, this.password);
+};
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+};
 
 export default mongoose.model('User', UserSchema);
